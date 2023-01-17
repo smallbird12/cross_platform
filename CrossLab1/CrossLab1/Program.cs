643bbbaaced0e3921ead2aba4dd26e15d791e22a
smallbird12 <121452217+smallbird12@users.noreply.github.com>
2023-01-17 19:46:44 +0300

    Add files via upload

---
+++ CrossLab1/CrossLab1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            StreamReader sr = new StreamReader("C:/Users/OneDrive/Desktop/CrossPlat/CrossPlat/Cross_lab1/CrossLab1/input.txt");
+            line = sr.ReadLine();
+            string[] arr = line.Split(" ");
+            int n = Convert.ToInt32(arr[2]);
+            int blue = Convert.ToInt32(arr[0]);
+            int red = Convert.ToInt32(arr[1]);
+            int result = (blue + red) * (blue + red) * n;
+            Console.WriteLine(result);
+            sr.Close();
+            Console.ReadKey();
+        }
+    }
+}
