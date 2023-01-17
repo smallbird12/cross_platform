0751538b4236cb8062e306e2f089bf604a8221e6
smallbird12 <121452217+smallbird12@users.noreply.github.com>
2023-01-17 19:51:06 +0300

    Add files via upload

---
+++ Croos_lab2/ConsoleApp7/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+            string lines;
+            StreamReader sr = new StreamReader("C:/Users/OneDrive/Desktop/CrossPlat/CrossPlat/Croos_lab2/input.txt");
+            lines = sr.ReadLine();
+            int N = Convert.ToInt32(lines);
+            double n = Convert.ToDouble(lines);
+            double k = n / 3;
+            int K = N / 3;
+            if (k == K)
+            {
+                double result = Math.Pow(2, Convert.ToDouble(K - 1));
+                Console.WriteLine(result);
+            }
+            else
+            {
+                double result = Math.Pow(2, Convert.ToDouble(K));
+                Console.WriteLine(result);
+            }
+            sr.Close();
+            Console.ReadKey();
+            
+        }
+    }
+}
+      
